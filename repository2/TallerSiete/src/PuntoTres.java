084bad397b89acee09c5496cd5da0a995012f51a
Andres110903 <112124106+Andres110903@users.noreply.github.com>
2022-10-07 21:17:35 -0500

    Add files via upload

---
+++ repository2/TallerSiete/src/PuntoTres.java
@@ -0,0 +1,13 @@
+
+public class PuntoTres {
+	public static void main(String[] args) {
+		String cadena = "Hello";
+		int numero;
+		
+		try {
+			numero = Integer.parseInt(cadena);
+			}catch (NumberFormatException e) {
+				System.out.print("No es un número, es una cadena");
+			}
+	}
+}
